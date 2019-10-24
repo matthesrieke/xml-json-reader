0d4842d7e8e7b26c8ed18324cfaec45018f50a40
mohammedsaad <mohammed912009@hotmail.com>
2019-10-24 14:36:42 +0200

    Update Measurements.java
    added annotations required by JAXB with refactoring for the names of the methods

--- src/main/java/org/n52/datareader/model/Measurements.java
+++ src/main/java/org/n52/datareader/model/Measurements.java
@@ -1,15 +1,20 @@
 package org.n52.datareader.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
-
+@XmlRootElement(name = "measurements")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Measurements {
-
+    @XmlElement(name = "measurement", type = Measurement.class)
     private List<Measurement> measurementList = new ArrayList<>();
-    public void setMeasurementList(List<Measurement> measurementList) {
+    public void setMeasurements(List<Measurement> measurementList) {
         this.measurementList = measurementList;
     }
-    public List<Measurement> getMeasurementList() {
+    public List<Measurement> getMeasurements() {
         return measurementList;
     }
 
