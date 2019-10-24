669d454dbbee7afac53aabadc377c2531fe22369
mohammedsaad <mohammed912009@hotmail.com>
2019-10-24 14:51:45 +0200

    Create XmlParser.java

---
+++ src/main/java/org/n52/datareader/service/XmlParser.java
@@ -0,0 +1,30 @@
+package org.n52.datareader.service;
+
+import org.n52.datareader.model.Measurements;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class XmlParser  {
+
+
+    private Measurements measurements;
+
+    public void unmarshall(Object in) throws JAXBException, IOException {
+        JAXBContext context = JAXBContext.newInstance(Measurements.class);
+        if (in instanceof File)
+             measurements = (Measurements) context.createUnmarshaller().unmarshal(new FileReader((File)in));
+        else if (in instanceof InputStream)
+            measurements = (Measurements) context.createUnmarshaller().unmarshal((InputStream) in);
+    }
+
+    public Measurements getContent() {
+        return measurements;
+    }
+
+}
