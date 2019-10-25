819159a487a7b8912dc5bfc2929fc7bdd05e8fbc
mohammedsaad <mohammed912009@hotmail.com>
2019-10-25 14:24:01 +0200

    Update DatareaderApplication.java

--- src/main/java/org/n52/datareader/DatareaderApplication.java
+++ src/main/java/org/n52/datareader/DatareaderApplication.java
@@ -23,9 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.MimeType;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
 
 
 @SpringBootApplication
